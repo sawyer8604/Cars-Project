9d25f0c9cf94e2da1699bdb24580ac71cfa7774f
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-09-09 15:21:44 +0300

    Update

--- Data/Cars.Data.Models/Area.cs
+++ Data/Cars.Data.Models/Area.cs
@@ -13,7 +13,7 @@
             this.Towns = new HashSet<Towns>();
         }
 
-        public int Name { get; set; }
+        public string Name { get; set; }
 
         public ICollection<Car> Cars { get; set; }
 
