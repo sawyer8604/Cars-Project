a6da5bf23c7b1326fd041063f1f43a89ef43e72c
sawyer8604@yahoo.com <sawyer8604@yahoo.com>
2023-09-20 18:49:02 +0300

    Add PartialView for paging

--- Web/Cars.Web.ViewModels/Car/CarsListViewModel.cs
+++ Web/Cars.Web.ViewModels/Car/CarsListViewModel.cs
@@ -5,27 +5,11 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Cars.Web.ViewModels;
 
-	public class CarsListViewModel
+    public class CarsListViewModel : PagingViewModel
 	{
         public IEnumerable<CarInListViewModel> Cars { get; set; }
-
-        public int PageNumber { get; set; }
-
-        public bool HasPreviousPage => this.PageNumber > 1;
-
-		public bool HasNextPage => this.PageNumber < this.PagesCount;
-
-
-		public int PreviousPageNumber => this.PageNumber - 1;
-
-		public int NextPageNumber => this.PageNumber + 1;
-
-		public int PagesCount => (int)Math.Ceiling((double)this.CarsCount / this.ItemsPerPage);
-
-        public int CarsCount { get; set; }
-
-		public int ItemsPerPage { get; set; }
 
 	}
 }
